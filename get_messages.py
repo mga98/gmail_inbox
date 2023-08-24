e3f10fec4af3003d704cd368206555ab915ca237
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-08-24 18:44:40 -0300

    first commit

---
+++ get_messages.py
@@ -0,0 +1,81 @@
+import csv
+
+
+class Messages:
+    def __init__(self, service):
+        self.service = service
+        self.results = self.filter()
+        self.messages = self.results.get('messages')
+
+    def filter(
+        self,
+        filter='',
+        userId='me',
+        includeSpamTrash=False,
+        labelIds='INBOX',
+    ) -> object:
+        '''
+        Filter the email that will be displayed
+        '''
+        return self.service.users().messages().list(
+            userId=userId,
+            includeSpamTrash=includeSpamTrash,
+            labelIds=labelIds,
+            q=f'category:primary subject:{filter}',
+        ).execute()
+
+    def get_messages_header(self) -> list | str:
+        '''
+        Return a list of emails subjects and senders
+        '''
+        if not self.messages:
+            return 'Você não tem nenhuma mensagem ainda.'
+
+        messages_header = list()
+
+        for msg in self.messages:
+            txt = self.service.users().messages().get(
+                userId='me', id=msg['id']
+            ).execute()
+
+            try:
+                payload = txt['payload']
+                headers = payload['headers']
+
+                for d in headers:
+                    if d['name'] == 'Subject':
+                        subject = d['value']
+
+                    if d['name'] == 'From':
+                        sender = d['value']
+
+                messages_header.append({
+                    'subject': subject,
+                    'from': sender
+                })
+
+            except Exception:
+                pass
+
+        return messages_header
+
+    def get_columns(self, messages: list) -> list:
+        '''
+        Return a list of keys to be columns in csv file
+        '''
+        return list(messages[0])
+
+    def save_csv(self, messages: list, file_name: str = 'file.csv') -> None:
+        '''
+        Save the messages dict into a csv file
+        '''
+        columns = self.get_columns(messages)
+
+        with open(
+            file_name, 'w', newline='', encoding='utf-8'
+        ) as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=columns)
+            writer.writeheader()
+
+            for message in messages:
+                writer.writerow(message)
---
+++ main.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+from api import start_api
+from get_messages import Messages
+
+
+def main():
+    # Call the Gmail API
+    service = start_api('credentials.json')
+    messages = Messages(service)
+
+    messages_header = messages.filter(filter='nito')
+    messages_header = messages.get_messages_header()
+    print(messages_header)
+
+
+if __name__ == '__main__':
+    main()
