1b260d3be51b7ca149e85a685509f5a63b428ba9
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-09-25 10:27:40 -0300

    empty filtered emails bug fixed

--- src/controlers/deep_filter.py
+++ src/controlers/deep_filter.py
@@ -1,13 +1,16 @@
 def deep_filter(emails: list, filters: list) -> list:
     filtered_emails = []
 
-    for email in emails:
-        lower_subject = email['subject'].lower()
-        lower_from = email['from'].lower()
+    if emails:
+        for email in emails:
+            lower_subject = email['subject'].lower()
+            lower_from = email['from'].lower()
 
-        for filter in filters:
-            if filter.lower() in lower_subject or filter.lower() in lower_from:
-                filtered_emails.append(email)
-                break
+            for filter in filters:
+                filter = filter.lower()
+
+                if filter in lower_subject or filter in lower_from:
+                    filtered_emails.append(email)
+                    break
 
     return filtered_emails
